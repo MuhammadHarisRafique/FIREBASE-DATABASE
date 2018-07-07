be96e6d1f97d8393325baf89b85ddee944d76a33
Haris <haris.math@gmail.com>
2018-07-07 19:08:49 +0500

    save and update data into firebASE

---
+++ Pods/Target Support Files/Pods-RealDatabase/Pods-RealDatabase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealDatabase : NSObject
+@end
+@implementation PodsDummy_Pods_RealDatabase
+@end
--- RealDatabase/ViewController.swift
+++ RealDatabase/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt_name: UITextField!
+    
+    @IBOutlet weak var txt_jobname: UITextField!
+    var ref: DatabaseReference!
+    
+   
+    @IBAction func save_user(_ sender: UIButton) {
+        
+        self.ref.child("Person").child(self.txt_name.text!).setValue(self.txt_jobname.text!)
+        
+    }
+    @IBAction func update_user(_ sender: UIButton) {
+        
+        
+        self.ref.child("Person").child(self.txt_name.text!).setValue(self.txt_jobname.text!)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         ref = Database.database().reference()
+   
+        ref.child("Person").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            print(value)
+            
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
